4eaf1f18bd20f110b4f88a95e7d903531ef5c96a
es-e2b <ensu1997@naver.com>
2025-11-16 17:51:44 +0900

    feat(character): 캐릭터 이동 시 스프라이트 방향 전환 구현

--- Assets/Scripts/Runtime/Domains/Characters/PlayerMovement.cs
+++ Assets/Scripts/Runtime/Domains/Characters/PlayerMovement.cs
@@ -6,6 +6,7 @@ namespace Runtime.Domains.Characters
     public class PlayerMovement : MonoBehaviour
     {
         [field: SerializeField] private Rigidbody2D _rigidbody;
+        [field: SerializeField] private SpriteRenderer _spriteRenderer;
         [field: SerializeField] private float _speed;
         public Vector2 Direction { get; set; }
 
@@ -13,6 +14,14 @@ namespace Runtime.Domains.Characters
         {
             _rigidbody.MovePosition(_rigidbody.position + Direction.normalized * _speed * Time.fixedDeltaTime);
             _rigidbody.linearVelocity = Vector2.zero;
+        }
+
+        private void LateUpdate()
+        {
+            if(Direction != Vector2.zero)
+            {
+                _spriteRenderer.flipX = Direction.x < 0;
+            }
         }
     }
 }
